5295783965abe560ed6b95d8510a29d43d247f84
dev-quanty <46728043+dev-quanty@users.noreply.github.com>
2023-11-22 11:03:02 +0100

    Add Runge-Kutta 4th order method

--- seird.py
+++ seird.py
@@ -41,12 +41,43 @@ class SEIRD:
                 It1 = It + dt * (alpha * Et - gamma * It)
                 Rt1 = Rt + dt * gamma * (1 - theta) * It
                 Dt1 = Dt + dt * gamma * theta * It
-            else:
+            elif method.lower() == "implicit":
                 St1 = St / (1 + dt * beta * It)
                 Et1 = (Et + dt * beta * It * St1) / (1 + dt * alpha)
                 It1 = (It + dt * alpha * Et1) / (1 + dt * gamma)
                 Rt1 = Rt + dt * gamma * (1 - theta) * It1
                 Dt1 = Dt + dt * gamma * theta * It1
+            else:
+                Sk1 = -beta * It * St
+                Ek1 = beta * It * St - alpha * Et
+                Ik1 = alpha * Et - gamma * It
+                Rk1 = gamma * (1 - theta) * It
+                Dk1 = gamma * theta * It
+
+                Sk2 = -beta * (St + (Sk1 * dt) / 2) * (It + (Ik1 * dt) / 2)
+                Ek2 = beta * (St + (Sk1 * dt) / 2) * (It + (Ik1 * dt) / 2) - alpha * (Et + (Ek1 * dt) / 2)
+                Ik2 = alpha * (Et + (Ek1 * dt) / 2) - gamma * (It + (Ik1 * dt) / 2)
+                Rk2 = (1 - theta) * gamma * (It + (Ik1 * dt) / 2)
+                Dk2 = theta * gamma * (It + (Ik1 * dt) / 2)
+
+                Sk3 = -beta * (St + (Sk2 * dt) / 2) * (It + (Ik2 * dt) / 2)
+                Ek3 = beta * (St + (Sk2 * dt) / 2) * (It + (Ik2 * dt) / 2) - alpha * (Et + (Ek2 * dt) / 2)
+                Ik3 = alpha * (Et + (Ek2 * dt) / 2) - gamma * (It + (Ik2 * dt) / 2)
+                Rk3 = (1 - theta) * gamma * (It + (Ik2 * dt) / 2)
+                Dk3 = theta * gamma * (It + (Ik2 * dt) / 2)
+
+                Sk4 = -beta * (St + Sk3 * dt) * (It + Ik3 * dt)
+                Ek4 = beta * (St + Sk3 * dt) * (It + Ik3 * dt) - alpha * (Et + Ek3 * dt)
+                Ik4 = alpha * (Et + Ek3 * dt) - gamma * (It + Ik3 * dt)
+                Rk4 = (1 - theta) * gamma * (It + Ik3 * dt)
+                Dk4 = theta * gamma * (It + Ik3 * dt)
+
+                St1 = St + dt * (Sk1 + 2 * Sk2 + 2 * Sk3 + Sk4) / 6
+                Et1 = Et + dt * (Ek1 + 2 * Ek2 + 2 * Ek3 + Ek4) / 6
+                It1 = It + dt * (Ik1 + 2 * Ik2 + 2 * Ik3 + Ik4) / 6
+                Rt1 = Rt + dt * (Rk1 + 2 * Rk2 + 2 * Rk3 + Rk4) / 6
+                Dt1 = Dt + dt * (Dk1 + 2 * Dk2 + 2 * Dk3 + Dk4) / 6
+
             df.loc[j+1] = [St1, Et1, It1, Rt1, Dt1, t1]
             j += 1
             try: linnorm = np.linalg.norm(np.array([St1, Et1, It1, Rt1, Dt1]) - np.array([St, Et, It, Rt, Dt]))
