c6b45432d0ff33196cd56b428468a57ca302ee6d
Lisabeth <106864761+PeppermintCoding123@users.noreply.github.com>
2023-11-29 10:52:09 +0100

    Update make_data.py and fit_model.py to with all compartments

--- fit_model.py
+++ fit_model.py
@@ -4,13 +4,19 @@ import csv
 import scipy.optimize
 from scipy.integrate import odeint
 
-## read data
-
+# read data
 time = []
 S_pop = []
 E_pop = []
 I_pop = []
+Q_e_pop = []
+Q_pop = []
+H_pop = []
+F_pop = []
+R_pop = []
+D_pop = []
 
+# add data to population arrays
 with open("population_data.csv") as file:
     reader = csv.reader(file, delimiter=',')
 
@@ -22,52 +28,86 @@ with open("population_data.csv") as file:
         S_pop.append(float(row[1]))
         E_pop.append(float(row[2]))
         I_pop.append(float(row[3]))
+        Q_e_pop.append(float(row[4]))
+        Q_pop.append(float(row[5]))
+        H_pop.append(float(row[6]))
+        F_pop.append(float(row[7]))
+        R_pop.append(float(row[8]))
+        D_pop.append(float(row[9]))
 
-f, (ax1, ax2, ax3) = plt.subplots(3)
+# define Plots
+f, (ax1, ax2, ax3, ax4, ax5, ax6, ax7, ax8, ax9) = plt.subplots(9)
 
 line1 = ax1.scatter(time, S_pop, c="b")
 line2 = ax2.scatter(time, E_pop, c="r")
 line3 = ax3.scatter(time, I_pop, c="y")
+line4 = ax4.scatter(time, Q_e_pop, c="c")
+line5 = ax5.scatter(time, Q_pop, c="m")
+line6 = ax6.scatter(time, H_pop, c="pink")
+line7 = ax7.scatter(time, F_pop, c="grey")
+line8 = ax8.scatter(time, R_pop, c="g")
+line9 = ax9.scatter(time, D_pop, c="k")
 
 ax1.set_ylabel("S")
 ax2.set_ylabel("E")
-ax2.set_ylabel("I")
-ax3.set_xlabel("Time")
+ax3.set_ylabel("I")
+ax4.set_ylabel("Q_e")
+ax5.set_ylabel("Q")
+ax6.set_ylabel("H")
+ax7.set_ylabel("F")
+ax8.set_ylabel("R")
+ax9.set_ylabel("D")
+ax9.set_xlabel("Time")
 
 
-#
-# plt.show()
-
-
+# Simulation method
 def sim(variables, t, params):
-    # set parameters
+    # set parameters - should be calculated with real-world Data
     i = 0.35
     h_s = 0.1
     f = 0.2
     e = 0.2
+    q_e = 0.2
+    q_i = 0.2
     y_i = 0.02
+    y_q = 0.02
+    y_h = 0.02
+    h = 0.1
+    p_i = p_q = p_h = 0.8
 
     # Compartments
     S = variables[0]
     E = variables[1]
     I = variables[2]
-    # R = variables[3]
-    # D = variables[4]
+    Q_e = variables[3]
+    Q = variables[4]
+    H = variables[5]
+    F = variables[6]
+    R = variables[7]
+    D = variables[8]
 
     k_i = params[0]
     k_h = params[1]
     k_f = params[2]
     d = params[3]
 
-    dsdt = -k_i * i * S * I
-    dedt = k_i * i * S * I - e * E
-    didt = e * E - y_i * I
+    # the ODE
+    dsdt = -k_i * i * S * I - k_h * h_s * S * H - k_f * f * S * F
+    dedt = k_i * i * S * I + k_h * h_s * S * H + k_f * f * S * F - e * E - q_e * E
+    didt = e * E - y_i * I - h * I - q_i * I
+    dq_edt = q_e * E - e * Q_e
+    dqdt = e * Q_e + q_i * I - y_q * Q - h * Q
+    dhdt = h * I + h * Q - y_h * H
+    dfdt = p_i * y_i * I + p_q * y_q * Q + p_h * y_h * H - d * F
+    drdt = (1 - p_i) * y_i * I + (1 - p_q) * y_q * Q + (1 - p_h) * y_h * H
+    dddt = d * F
 
-    return ([dsdt, dedt, didt])
+    return ([dsdt, dedt, didt, dq_edt, dqdt, dhdt, dfdt, drdt, dddt])
 
 
-def loss_function(params, time, S_pop, E_pop, I_pop):
-    y0 = [S_pop[0], E_pop[0], I_pop[0]]
+# Calculate loss for parameter estimation
+def loss_function(params, time, S_pop, E_pop, I_pop, Q_e_pop, Q_pop, H_pop, F_pop, R_pop, D_pop):
+    y0 = [S_pop[0], E_pop[0], I_pop[0], Q_e_pop[0], Q_pop[0], H_pop[0], F_pop[0], R_pop[0], D_pop[0]]
 
     t = np.linspace(time[0], time[-1], num=len(time))
 
@@ -85,26 +125,51 @@ def loss_function(params, time, S_pop, E_pop, I_pop):
         data_I = E_pop[i]
         model_I = output[i, 2]
 
-        res = (data_S - model_S) ** 2 + (data_E - model_E) ** 2 + (data_I - model_I) ** 2
+        data_Q_e = S_pop[i]
+        model_Q_e = output[i, 3]
+
+        data_Q = E_pop[i]
+        model_Q = output[i, 4]
+
+        data_H = E_pop[i]
+        model_H = output[i, 5]
+
+        data_F = S_pop[i]
+        model_F = output[i, 6]
+
+        data_R = E_pop[i]
+        model_R = output[i, 7]
+
+        data_D = E_pop[i]
+        model_D = output[i, 8]
+
+        res = (data_S - model_S) ** 2 + (data_E - model_E) ** 2 + (data_I - model_I) ** 2 \
+              + (data_Q_e - model_Q_e) ** 2 + (data_Q - model_Q) ** 2 + (data_H - model_H) \
+              + (data_F - model_F) ** 2 + (data_R - model_R) ** 2 + (data_D - model_D)
 
         loss += res
 
     return (loss)
 
 
-params0 = np.array([1, 1, 1, 1])
-minimum = scipy.optimize.fmin(loss_function, params0, args=(time, S_pop, E_pop, I_pop))
+# optimize
+params0 = np.array([1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
+minimum = scipy.optimize.fmin(loss_function, params0,
+                              args=(time, S_pop, E_pop, I_pop, Q_e_pop, Q_pop, H_pop, F_pop, R_pop, D_pop))
 
-print(minimum)
-
+# plot curves
 k_i_fit = minimum[0]
 k_h_fit = minimum[1]
 k_f_fit = minimum[2]
 d_fit = minimum[3]
+print("k_i = " + str(k_i_fit))
+print("k_h = " + str(k_h_fit))
+print("k_f = " + str(k_f_fit))
+print("d = " + str(d_fit))
 
 params = [k_i_fit, k_h_fit, k_f_fit, d_fit]
 
-y0 = [S_pop[0], E_pop[0], I_pop[0]]
+y0 = [S_pop[0], E_pop[0], I_pop[0], Q_e_pop[0], Q_pop[0], H_pop[0], F_pop[0], R_pop[0], D_pop[0]]
 
 t = np.linspace(time[0], time[-1], num=1000)
 
@@ -113,10 +178,22 @@ output = odeint(sim, y0, t, args=(params,))
 line1, = ax1.plot(t, output[:, 0], color="b")
 line2, = ax2.plot(t, output[:, 1], color="r")
 line3, = ax3.plot(t, output[:, 2], color="y")
+line4, = ax4.plot(t, output[:, 3], color="c")
+line5, = ax5.plot(t, output[:, 4], color="m")
+line6, = ax6.plot(t, output[:, 5], color="pink")
+line7, = ax7.plot(t, output[:, 6], color="grey")
+line8, = ax8.plot(t, output[:, 7], color="g")
+line9, = ax9.plot(t, output[:, 8], color="k")
 
 ax1.set_ylabel("S")
 ax2.set_ylabel("E")
 ax3.set_ylabel("I")
+ax4.set_ylabel("Q_e")
+ax5.set_ylabel("Q")
+ax6.set_ylabel("H")
+ax7.set_ylabel("F")
+ax8.set_ylabel("R")
+ax9.set_ylabel("D")
 ax3.set_xlabel("Time")
 
 plt.show()
--- make_data.py
+++ make_data.py
@@ -1,15 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
 from scipy.integrate import odeint
 
 # define starting population
-y0 = [10, 1, 0, 0, 0]
+y0 = [10, 1, 0, 0, 0, 0, 0, 0, 0]
 
 t = np.linspace(0, 100, num=1001)
 
 # variables that our fitting should approximate
-k_i = 1.1
-k_h = 0.4
+k_i = 0.4
+k_h = 0.1
 k_f = 0.1
 d = 0.4
 
@@ -17,30 +18,46 @@ params = [k_i, k_h, k_f, d]
 
 
 def sim(variables, t, params):
-    # set parameters
+    # set parameters - should be calculated with real-world Data
     i = 0.35
     h_s = 0.1
     f = 0.2
     e = 0.2
+    q_e = 0.2
+    q_i = 0.2
     y_i = 0.02
+    y_q = 0.02
+    y_h = 0.02
+    h = 0.1
+    p_i = p_q = p_h = 0.8
 
     # Compartments
     S = variables[0]
     E = variables[1]
     I = variables[2]
-    R = variables[3]
-    D = variables[4]
+    Q_e = variables[3]
+    Q = variables[4]
+    H = variables[5]
+    F = variables[6]
+    R = variables[7]
+    D = variables[8]
 
     k_i = params[0]
     k_h = params[1]
     k_f = params[2]
     d = params[3]
 
-    dsdt = -k_i * i * S * I
-    dedt = k_i * i * S * I - e * E
-    didt = e * E - y_i * I
+    dsdt = -k_i * i * S * I - k_h * h_s * S * H - k_f * f * S * F
+    dedt = k_i * i * S * I + k_h * h_s * S * H + k_f * f * S * F - e * E - q_e * E
+    didt = e * E - y_i * I - h * I - q_i * I
+    dq_edt = q_e * E - e * Q_e
+    dqdt = e * Q_e + q_i * I - y_q * Q - h * Q
+    dhdt = h * I + h * Q - y_h * H
+    dfdt = p_i * y_i * I + p_q * y_q * Q + p_h * y_h * H - d * F
+    drdt = (1 - p_i) * y_i * I + (1 - p_q) * y_q * Q + (1 - p_h) * y_h * H
+    dddt = d * F
 
-    return ([dsdt, dedt, didt, 0, 0])
+    return ([dsdt, dedt, didt, dq_edt, dqdt, dhdt, dfdt, drdt, dddt])
 
 
 y = odeint(sim, y0, t, args=(params,))
@@ -49,19 +66,37 @@ data_t = []
 data_S = []
 data_E = []
 data_I = []
+data_Q_e = []
+data_Q = []
+data_H = []
+data_F = []
+data_R = []
+data_D = []
 
 for i in range(t.shape[0]):
     print(t[i])
     print(y[i, 0])
     print(y[i, 1])
     print(y[i, 2])
+    print(y[i, 3])
+    print(y[i, 4])
+    print(y[i, 5])
+    print(y[i, 6])
+    print(y[i, 7])
+    print(y[i, 8])
     print()
 
     if i % 20 == 0:
         data_t.append(t[i])
         data_S.append(np.random.normal(loc=y[i, 0], scale=0.2, ))
         data_E.append(np.random.normal(loc=y[i, 1], scale=0.2, ))
-        data_I.append(np.random.normal(loc=y[i, 1], scale=0.2, ))
+        data_I.append(np.random.normal(loc=y[i, 2], scale=0.2, ))
+        data_Q_e.append(np.random.normal(loc=y[i, 3], scale=0.2, ))
+        data_Q.append(np.random.normal(loc=y[i, 4], scale=0.2, ))
+        data_H.append(np.random.normal(loc=y[i, 5], scale=0.2, ))
+        data_F.append(np.random.normal(loc=y[i, 6], scale=0.2, ))
+        data_R.append(np.random.normal(loc=y[i, 7], scale=0.2, ))
+        data_D.append(np.random.normal(loc=y[i, 8], scale=0.2, ))
 
 print(y.shape)
 print(t.shape)
@@ -69,28 +104,40 @@ print(t.shape)
 for row in data_t:
     print(row)
 
-# sys.exit()
-
-f, (ax1, ax2, ax3) = plt.subplots(3)
-
-line1 = ax1.scatter(data_t, data_S, c="b")
-line2 = ax2.scatter(data_t, data_E, c="r")
-line3 = ax3.scatter(data_t, data_I, c="y")
-
-ax1.set_ylabel("S")
-ax2.set_ylabel("E")
-ax3.set_ylabel("I")
-ax3.set_xlabel("Time (years)")
-plt.savefig("population_data.pdf")
-
-plt.show()
-
 # making CSV
 f = open('population_data.csv', "w")
 
 f.write("year,data_S,data_E,data_I\n")
 
 for i in range(len(data_t)):
-    f.write("%s,%s,%s,%s\n" % (data_t[i], data_S[i], data_E[i], data_I[i]))
+    f.write("%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n" % (data_t[i], data_S[i], data_E[i], data_I[i], data_Q_e[i], data_Q[i],
+                                                 data_H[i], data_F[i], data_R[i], data_D[i]))
 
 f.close()
+
+# Visualize
+f, (ax1, ax2, ax3, ax4, ax5, ax6, ax7, ax8, ax9) = plt.subplots(9)
+
+line1 = ax1.scatter(data_t, data_S, c="b")
+line2 = ax2.scatter(data_t, data_E, c="r")
+line3 = ax3.scatter(data_t, data_I, c="y")
+line4 = ax4.scatter(data_t, data_Q_e, c="c")
+line5 = ax5.scatter(data_t, data_Q, c="m")
+line6 = ax6.scatter(data_t, data_H, c="pink")
+line7 = ax7.scatter(data_t, data_F, c="grey")
+line8 = ax8.scatter(data_t, data_R, c="g")
+line9 = ax9.scatter(data_t, data_D, c="k")
+
+ax1.set_ylabel("S")
+ax2.set_ylabel("E")
+ax3.set_ylabel("I")
+ax4.set_ylabel("Q_e")
+ax5.set_ylabel("Q")
+ax6.set_ylabel("H")
+ax7.set_ylabel("F")
+ax8.set_ylabel("R")
+ax9.set_ylabel("D")
+ax9.set_xlabel("Time")
+plt.savefig("population_data.pdf")
+
+plt.show()
