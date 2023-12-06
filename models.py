390e8f7ac141de980c8b52f69b0a4f864dff3ffc
Lisabeth <106864761+PeppermintCoding123@users.noreply.github.com>
2023-12-06 09:08:22 +0100

    rename model parameters to fit format of theory

--- models.py
+++ models.py
@@ -2,35 +2,35 @@ import numpy as np
 
 
 def sir(y, t, args):
-    beta, gamma = args
+    ki, yi = args
     S, I, R = y
-    dSdt = -beta * S * I
-    dIdt = beta * S * I - gamma * I
-    dRdt = gamma * I
+    dSdt = -ki * S * I
+    dIdt = ki * S * I - yi * I
+    dRdt = yi * I
     return np.array([dSdt, dIdt, dRdt])
 
 
 def seird(y, t, args):
-    beta, alpha, gamma, theta = args
+    ki, e, yi, pi = args
     S, E, I, R, D = y
-    dSdt = -beta * S * I
-    dEdt = beta * S * I - alpha * E
-    dIdt = alpha * E - gamma * I
-    dRdt = gamma * (1 - theta) * I
-    dDdt = gamma * theta * I
+    dSdt = -ki * S * I
+    dEdt = ki * S * I - e * E
+    dIdt = e * E - yi * I
+    dRdt = yi * (1 - pi) * I
+    dDdt = yi * pi * I
     return np.array([dSdt, dEdt, dIdt, dRdt, dDdt])
 
 
 def seirqhfd(y, t, args):
-    ki, kh, kf, e, qe, qi, h, yi, yq, yh, pi, pq, ph, d = args
+    ki, kh, kf, e, qe, qi, h, yi, yh, pi, ph, d = args
     S, E, I, QE, QI, H, R, F, D = y
     dSdt = -ki * S * I - kh * S * H - kf * S * F
     dEdt = ki * S * I + kh * S * H + kf * S * F - e * E - qe * E
     dIdt = e * E - qi * I - yi * I - h * I
     dQEdt = qe * E - e * QE
-    dQIdt = e * QE + qi * I - yq * QI - h * QI
+    dQIdt = e * QE + qi * I - yi * QI - h * QI
     dHdt = h * I + h * QI - yh * H
-    dRdt = yi * (1 - pi) * I + yq * (1 - pq) * QI + yh * (1 - ph) * H
-    dFdt = yi * pi * I + yq * pq * QI + yh * ph * H - d * F
+    dRdt = yi * (1 - pi) * I + yi * (1 - pi) * QI + yh * (1 - ph) * H
+    dFdt = yi * pi * I + yi * pi * QI + yh * ph * H - d * F
     dDdt = d * F
     return np.array([dSdt, dEdt, dIdt, dQEdt, dQIdt, dHdt, dRdt, dFdt, dDdt])
