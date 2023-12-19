30fb96da3883dd0a7c15532ae6f8227bfea59f89
Lisabeth <106864761+PeppermintCoding123@users.noreply.github.com>
2023-12-19 16:40:56 +0100

    Add content to test_brick to fit format of given ODESolver-functions BenchmarkTest.py

--- test/BenchmarkTest.py
+++ test/BenchmarkTest.py
@@ -1,24 +1,50 @@
-# TODO: Testing ODESolver.py functions
 import numpy as np
 import perfplot
+from tabulate import tabulate
+from src.ODESolver import forwardEuler, backwardEuler, RK4
 
 
 def test_brick():
+    def printPretty(out):
+        data = []
+        headers = ["n"] + out.labels
+        for n, t in zip(out.n_range, out.timings_s.T):
+            lst = [str(n)] + [str(tt) for tt in t]
+            data.append(lst)
+
+        print(tabulate(data, headers=headers, tablefmt='psql'))
+
+    # The function - preferably ODE - to calculate --- This can be replaced with functions from models.py
+    def func(y, t, args):
+        res = 0
+        for i in args:
+            res += i + y
+            res /= 20
+        return res
+
+    def setup(n):
+        y0 = np.random.rand(n)
+        t = np.linspace(0, 1, n)
+        args = np.random.rand(n)
+        return func, y0, t, args
+
+    # Define your kernels
+    kernels = [forwardEuler, backwardEuler, RK4]
+    labels = ["Forward Euler", "Backward Euler", "RK4"]
+
+    # Generate the performance plot
     out = perfplot.bench(
-        setup=lambda n: n,
-        kernels=[
-            # functions as names separated with comma
-            lambda n: list(np.arange(n))
-        ],
-        labels=[],  # function names as strings, separated with comma
-        n_range=[2 ** k for k in range(10)],  # log-scale
-        xlabel="size of input",
+        setup=setup,
+        kernels=kernels,
+        labels=labels,
+        n_range=[2 ** k + 10 for k in range(5)],
+        xlabel="iteration amount",
         equality_check=None
     )
-    print(out)
-    # out.show()
-    # out.save("perf.png", transparent=True, bbox_inches="tight")
 
+    # Print and save the output
+    printPretty(out)
+    out.save("benchPlot.png", transparent=True, bbox_inches="tight")
 
 if __name__ == "__main__":
     test_brick()
