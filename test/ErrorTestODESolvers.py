568cfc3a92e2d5094af213341f3393c7f21cd59d
Lisabeth <106864761+PeppermintCoding123@users.noreply.github.com>
2024-01-19 11:43:05 +0100

    Add euler functions and better plot

--- test/ErrorTestODESolvers.py
+++ test/ErrorTestODESolvers.py
@@ -16,7 +16,7 @@ def sysAnalytical(t_space):
 
 
 def testODEwith2D():
-    # Parameters
+    # parameters
     t_space = np.linspace(0., 5., 100)
     x_init = 2.
     y_init = 0.
@@ -24,19 +24,24 @@ def testODEwith2D():
     # analytical solution
     x_an_sol, y_an_sol = sysAnalytical(t_space)
 
-    # Function solution
-    RK4_sol = RK4(ode_sys, np.array([x_init, y_init]), t_space, 0)
-    x_num_sol = RK4_sol.T[0]
-    y_num_sol = RK4_sol.T[1]
+    # function solution
+    solRK4 = RK4(ode_sys, np.array([x_init, y_init]), t_space, 0)
+    solForEuler = forwardEuler(ode_sys, np.array([x_init, y_init]), t_space, 0)
+    solBackEuler = backwardEuler(ode_sys, np.array([x_init, y_init]), t_space, 0)
 
     # Plot
     plt.figure()
-    plt.plot(t_space, x_an_sol, '--', linewidth=2, label='analytical x')
-    plt.plot(t_space, y_an_sol, '--', linewidth=2, label='analytical y')
-    plt.plot(t_space, x_num_sol, linewidth=1, label='numerical x')
-    plt.plot(t_space, y_num_sol, linewidth=1, label='numerical y')
-    plt.title('System of 2 ODEs 1st order solved with different method')
+    plt.plot(t_space, x_an_sol, '--k', linewidth=2, label='analytical x')
+    plt.plot(t_space, y_an_sol, '--', color='dimgrey', linewidth=2, label='analytical y')
+    plt.plot(t_space, solRK4.T[0], color='darkviolet', linewidth=1, label='RK4 x')
+    plt.plot(t_space, solRK4.T[1], color='magenta', linewidth=1, label='RK4 y')
+    plt.plot(t_space, solForEuler.T[0], color='darkred', linewidth=1, label='forwardEuler x')
+    plt.plot(t_space, solForEuler.T[1], color='crimson', linewidth=1, label='forwardEuler y')
+    plt.plot(t_space, solBackEuler.T[0], color='mediumblue', linewidth=1, label='backwardEuler x')
+    plt.plot(t_space, solBackEuler.T[1], color='dodgerblue', linewidth=1, label='backwardEuler y')
+    plt.title('System of 2 ODEs 1st order solved with different methods')
     plt.xlabel('t')
+    plt.ylabel('value of equation')
     plt.legend()
     plt.show()
 
