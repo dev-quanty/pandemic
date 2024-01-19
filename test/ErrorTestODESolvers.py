55c96f52a975912ceb5b0eedb1fdf4b5508c7dfe
Lisabeth <106864761+PeppermintCoding123@users.noreply.github.com>
2024-01-19 12:00:41 +0100

    Add Error-Plot

--- test/ErrorTestODESolvers.py
+++ test/ErrorTestODESolvers.py
@@ -17,7 +17,7 @@ def sysAnalytical(t_space):
 
 def testODEwith2D():
     # parameters
-    t_space = np.linspace(0., 5., 100)
+    t_space = np.linspace(0., 10., 100)
     x_init = 2.
     y_init = 0.
 
@@ -29,7 +29,7 @@ def testODEwith2D():
     solForEuler = forwardEuler(ode_sys, np.array([x_init, y_init]), t_space, 0)
     solBackEuler = backwardEuler(ode_sys, np.array([x_init, y_init]), t_space, 0)
 
-    # Plot
+    # Plot solution
     plt.figure()
     plt.plot(t_space, x_an_sol, '--k', linewidth=2, label='analytical x')
     plt.plot(t_space, y_an_sol, '--', color='dimgrey', linewidth=2, label='analytical y')
@@ -40,6 +40,20 @@ def testODEwith2D():
     plt.plot(t_space, solBackEuler.T[0], color='mediumblue', linewidth=1, label='backwardEuler x')
     plt.plot(t_space, solBackEuler.T[1], color='dodgerblue', linewidth=1, label='backwardEuler y')
     plt.title('System of 2 ODEs 1st order solved with different methods')
+    plt.xlabel('t')
+    plt.ylabel('value of equation')
+    plt.legend()
+    plt.show()
+
+    # Plot error
+    plt.figure()
+    plt.plot(t_space, solRK4.T[0] - x_an_sol, color='darkviolet', linewidth=1, label='RK4 x')
+    plt.plot(t_space, solRK4.T[1] - y_an_sol, color='magenta', linewidth=1, label='RK4 y')
+    plt.plot(t_space, solForEuler.T[0] - x_an_sol, color='darkred', linewidth=1, label='forwardEuler x')
+    plt.plot(t_space, solForEuler.T[1] - y_an_sol, color='crimson', linewidth=1, label='forwardEuler y')
+    plt.plot(t_space, solBackEuler.T[0] - x_an_sol, color='mediumblue', linewidth=1, label='backwardEuler x')
+    plt.plot(t_space, solBackEuler.T[1] - y_an_sol, color='dodgerblue', linewidth=1, label='backwardEuler y')
+    plt.title('Error of ODE solvers with System of 2 ODEs 1st order')
     plt.xlabel('t')
     plt.ylabel('value of equation')
     plt.legend()
