8198128c26428ea51872fecc12bd83ee6f946ddc
Lisabeth <106864761+PeppermintCoding123@users.noreply.github.com>
2024-01-19 14:49:43 +0100

    Add Konsistenzfahlertest

--- test/ErrorTestODESolvers.py
+++ test/ErrorTestODESolvers.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from src.ODESolver import forwardEuler, backwardEuler, RK4
+from scipy.linalg import norm
 
 
 def ode_sys(XY, t, args):
@@ -60,5 +61,45 @@ def testODEwith2D():
     plt.show()
 
 
+def testODEkonsistenz():
+    values = [1, 5, 10, 25, 50, 75, 100]
+    n = len(values)
+    errRK4 = np.zeros(n)
+    errForEuler = np.zeros(n)
+    errBackEuler = np.zeros(n)
+    for i in range(n):
+        tau = values[i]
+
+        # parameters
+        t_space = np.linspace(0., 10., 10 * tau)
+        x_init = 2.
+        y_init = 0.
+
+        # analytical solution
+        x_an_sol, y_an_sol = sysAnalytical(t_space)
+
+        # function solution
+        solRK4 = RK4(ode_sys, np.array([x_init, y_init]), t_space, 0)
+        solForEuler = forwardEuler(ode_sys, np.array([x_init, y_init]), t_space, 0)
+        solBackEuler = backwardEuler(ode_sys, np.array([x_init, y_init]), t_space, 0)
+
+        errRK4[i] = norm(solRK4.T[0] - x_an_sol + solRK4.T[1] - y_an_sol, ord=np.inf)
+        errForEuler[i] = norm(solForEuler.T[0] - x_an_sol + solForEuler.T[1] - y_an_sol, ord=np.inf)
+        errBackEuler[i] = norm(solBackEuler.T[0] - x_an_sol + solBackEuler.T[1] - y_an_sol, ord=np.inf)
+
+    # Plot error
+    plt.figure()
+    plt.yscale("log")
+    plt.plot(values, errRK4, color='magenta', linewidth=1, label='RK4')
+    plt.plot(values, errForEuler, color='crimson', linewidth=1, label='forwardEuler')
+    plt.plot(values, errBackEuler, color='mediumblue', linewidth=1, label='backwardEuler')
+    plt.title('Konsistenzfehler')
+    plt.xlabel('tau - Zwischenschritte pro Schritt')
+    plt.ylabel('size of error of numerical calculation')
+    plt.legend()
+    plt.show()
+
+
 if __name__ == "__main__":
-    testODEwith2D()
+    # testODEwith2D()
+    testODEkonsistenz()
