9988e660ab34f6177964f9f931c1ac3820f1c45c
dev-quanty <46728043+dev-quanty@users.noreply.github.com>
2024-01-21 18:50:13 +0100

    Implement seirqhfd with shared parameters

--- src/models.py
+++ src/models.py
@@ -36,13 +36,28 @@ def seirqhfd(y, t, args):
     return np.array([dSdt, dEdt, dIdt, dQEdt, dQIdt, dHdt, dRdt, dFdt, dDdt])
 
 
+def seirqhfd_shared_weights(y, t, args):
+    ki, kh, kf, e, qe, h, yi, pi, d = args
+    S, E, I, QE, QI, H, R, F, D = y
+    dSdt = -ki * S * I - kh * S * H - kf * S * F
+    dEdt = ki * S * I + kh * S * H + kf * S * F - e * E - qe * E
+    dIdt = e * E - qe * I - yi * I - h * I
+    dQEdt = qe * E - e * QE
+    dQIdt = e * QE + qe * I - yi * QI - h * QI
+    dHdt = h * I + h * QI - yi * H
+    dRdt = yi * (1 - pi) * I + yi * (1 - pi) * QI + yi * (1 - pi) * H
+    dFdt = yi * pi * I + yi * pi * QI + yi * pi * H - d * F
+    dDdt = d * F
+    return np.array([dSdt, dEdt, dIdt, dQEdt, dQIdt, dHdt, dRdt, dFdt, dDdt])
+
+
 def letter_index(model, letters):
     func = model.__name__
     if func == "sir":
         model_letters = list("SIR")
     elif func == "seird":
         model_letters = list("SEIRD")
-    elif func == "seirqhfd":
+    elif func == "seirqhfd" or func == "seirqhfd_shared_weights":
         model_letters = list("SEIQQHRFD")
         model_letters[3] = "QE"
         model_letters[4] = "QI"
