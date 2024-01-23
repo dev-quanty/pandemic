3aa717bf28958659fef8e4c5dc7961035e00e55d
Lisabeth <106864761+PeppermintCoding123@users.noreply.github.com>
2024-01-23 17:52:00 +0100

    Update fittingErrorTest.py with SEIRDQHF-Test

--- src/fittingErrorTest.py
+++ src/fittingErrorTest.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 from fitting import minloss
 from models import sir, seird, seirqhfd
 from ODESolver import solve
+import warnings
 
 
 def fittingErrorSIvsSIR():
@@ -70,7 +71,8 @@ def fittingErrorSEIRD():
 
     plt.close()
     plt.figure()
-    plt.title(f"Gewählte Parameter: {params}\nFitted: {fittedparams}\nFehler: {np.linalg.norm(y - ytest)}")
+    plt.title(
+        f"Gewählte Parameter: {params}\nFitted: {fittedparams}\nFehler pro Messung: {np.linalg.norm(y - ytest) / 100}")
     plt.plot(t, y[:, 0], color="blue", label='S [Wahr]')
     plt.plot(t, ytest[:, 0], color="cornflowerblue", label='S [Fit]')
     plt.plot(t, y[:, 1], color="teal", label='E [Wahr]')
@@ -87,6 +89,51 @@ def fittingErrorSEIRD():
     plt.show()
 
 
+def fittingErrorSEIRDQHF():
+    # suppress warnings
+    warnings.filterwarnings('ignore')
+    # Create true data
+    y0 = [9, 0, 1, 0, 0, 0, 0, 0, 0]
+    params = [0.3, 0.3, 0.03, 0.111, 0.6, 0.18, 0.3, 0.7,
+              0.9, 0.6, 0.3, 0.18]
+    t = np.linspace(0, 50, 100)
+    y = solve(seirqhfd, y0, t, args=params)
+
+    # Fit all parameters
+    fittedparams, loss = minloss(np.hstack([t.reshape(-1, 1), y[:, [2, 3, 4, 5, 7, 8]]]), seirqhfd,
+                                 [0.3, 0.3, 0.03, 0.111, None, 0.18, None, None, None, None, None, 0.18],
+                                 "hooke", "RK4", letters="I,QE,QI,H,F,D")
+    ytest = solve(seirqhfd, y0, t, args=fittedparams)
+
+    plt.close()
+    plt.figure()
+    plt.title(
+        f"Gewählte Parameter: {params}\nFitted: {fittedparams}\nFehler pro Messung: {np.linalg.norm(y - ytest) / 100}")
+    plt.plot(t, y[:, 0], color="blue", label='S [Wahr]')
+    plt.plot(t, ytest[:, 0], color="cornflowerblue", label='S [Fit]')
+    plt.plot(t, y[:, 1], color="teal", label='E [Wahr]')
+    plt.plot(t, ytest[:, 1], color="turquoise", label='E [Fit]')
+    plt.plot(t, y[:, 2], color="darkgoldenrod", label='I [Wahr]')
+    plt.plot(t, ytest[:, 2], color="gold", label='I [Fit]')
+    plt.plot(t, y[:, 3], color="cadetblue", label='QE [Wahr]')
+    plt.plot(t, ytest[:, 3], color="paleturquoise", label='QE [Fit]')
+    plt.plot(t, y[:, 4], color="darkkhaki", label='QI [Wahr]')
+    plt.plot(t, ytest[:, 4], color="khaki", label='QI [Fit]')
+    plt.plot(t, y[:, 5], color="chocolate", label='H [Wahr]')
+    plt.plot(t, ytest[:, 5], color="peru", label='H [Fit]')
+    plt.plot(t, y[:, 6], color="green", label='R [Wahr]')
+    plt.plot(t, ytest[:, 6], color="lime", label='R [Fit]')
+    plt.plot(t, y[:, 7], color="maroon", label='F [Wahr]')
+    plt.plot(t, ytest[:, 7], color="lightcoral", label='F [Fit]')
+    plt.plot(t, y[:, 8], color="black", label='D [Wahr]')
+    plt.plot(t, ytest[:, 8], color="dimgrey", label='D [Fit]')
+    plt.xlabel('t - Zeitabschnitte')
+    plt.ylabel('Population')
+    plt.legend(loc='upper right')
+    plt.show()
+
+
 if __name__ == "__main__":
     fittingErrorSIvsSIR()
     fittingErrorSEIRD()
+    fittingErrorSEIRDQHF()
